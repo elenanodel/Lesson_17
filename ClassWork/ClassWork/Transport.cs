774521a990c7f906dc05570278ff26dd37669420
User <User@WIN-4I8V0TPRSH5>
2017-09-22 22:24:08 +0300

    Init Commit

--- ClassWork/ClassWork/Transport.cs
+++ ClassWork/ClassWork/Transport.cs
@@ -87,7 +87,9 @@ namespace Domain
         public WaterTransport(int speed, string manufactor, double width, double height, Engine engine, int amount) : base(speed, manufactor, width, height, engine, amount)
         {
 
-        }        
+        }    
+        
+            
        
     }
 
@@ -153,22 +155,124 @@ namespace Domain
 
     public class AirPlane : AirTransport
     {
+        private double wingSpan;
+        private double takeOffWeight;
 
+        public AirPlane() : base()
+        {
+            wingSpan = 0;
+            takeOffWeight = 0;
+        }
+
+        public AirPlane (int speed, string manufactor, double width, double height, Engine engine, int amount, double wingSpan, double takeOffWeight): base(speed, manufactor, width, height, engine, amount)
+        {
+            this.wingSpan = wingSpan;
+            this.takeOffWeight = takeOffWeight;
+        }
+
+        public double WingSpan
+        {
+            get { return wingSpan; }
+            set { wingSpan = value; }
+        }
+
+        public double TakeOffWeight
+        {
+            get { return takeOffWeight; }
+            set { takeOffWeight = value; }
+        }
     }
 
     public class Ship : WaterTransport
     {
+        private double displacement;
+        private string navigationArea;
 
+        public Ship(): base()
+        {
+            displacement = 0;
+            navigationArea = "unknown";
+        }
+
+        public Ship (int speed, string manufactor, double width, double height, Engine engine, int amount, double displacement, string navigationArea) : base(speed, manufactor, width, height, engine, amount)
+        {
+            this.displacement = displacement;
+            this.navigationArea = navigationArea;
+        }
+
+        public double Displacement
+        {
+            get { return displacement; }
+            set { displacement = value; }
+        }
+
+        public string NavigationArea
+        {
+            get { return navigationArea; }
+            set { navigationArea = value; }
+        }
     }
 
     public class Train : LandTransport
     {
+        //  подвижность состава
+        private string rollingStock;
+        //  регулярность обращения: круглогодичный, летний, разовый
+        private string regular;
 
+        public Train() : base()
+        {
+            rollingStock = "unknown";
+            regular = "unknown";
+        }
+
+        public Train(int speed, string manufactor, double width, double height, Engine engine, int amount, string rollingStock, string regular) : base(speed, manufactor, width, height, engine, amount)
+        {
+            this.rollingStock = rollingStock;
+            this.regular = regular;
+        }
+
+        public string RollingStock
+        {
+            get { return rollingStock; }
+            set { rollingStock = value; }
+        }
+
+        public string Regular
+        {
+            get { return regular; }
+            set { regular = value; }
+        }
     }
 
     public class Bike : LandTransport
     {
+        private string model;
+        private string brakes;
 
+        public Bike() : base()
+        {
+            model = "unknown";
+            brakes = "unknown";
+        }
+
+        public Bike(int speed, string manufactor, double width, double height, Engine engine, int amount, string model, string brakes) : base(speed, manufactor, width, height, engine, amount)
+        {
+            this.model = model;
+            this.brakes = brakes;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Brakes
+        {
+            get { return brakes; }
+            set { brakes = value; }
+        }
     }
 
     public abstract class Engine
@@ -208,16 +312,64 @@ namespace Domain
 
     public class PetrolEngine : Engine
     {
+        private string blendingProcess; //  injector, carburetor
+        
+        public PetrolEngine() : base()
+        {
+            blendingProcess = "unknown";
+        }
 
+        public PetrolEngine(int power, string manufactor, string blendingProcess) : base(power, manufactor)
+        {
+            this.blendingProcess = blendingProcess;
+        }
+
+        public string BlendingProcess
+        {
+            get { return blendingProcess; }
+            set { blendingProcess = value; }
+        }
     }
 
     public class Disel : Engine
     {
+        private string construction;
 
+        public Disel () : base()
+        {
+            construction = "unknown";
+        }
+
+        public Disel(int power, string manufactor, string construction) : base(power, manufactor)
+        {
+            this.construction = construction;
+        }
+
+        public string Construction
+        {
+            get { return construction; }
+            set { construction = value; }
+        }
     }
 
     public class ReactiveEngine : Engine
     {
+        private string classes;
 
+        public ReactiveEngine() : base()
+        {
+            classes = "unknown";
+        }
+
+        public ReactiveEngine(int power, string manufactor, string classes) : base(power, manufactor)
+        {
+            this.classes = classes;
+        }
+
+        public string Classes
+        {
+            get { return classes; }
+            set { classes = value; }
+        }
     }
 }
